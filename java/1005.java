737322b26e32fcab0741f5477a07f71cb48556a3
Gisele Bezerra de Almeida <110948857+GiseleBezerra@users.noreply.github.com>
2024-07-20 18:16:52 -0300

    Média 1

---
+++ java/1005.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner entrada = new Scanner(System.in);
+    		double A = entrada.nextDouble();
+    		double B = entrada.nextDouble();
+    		double mediaNota = ((A * 3.5) + (B * 7.5)) / 11;
+    					
+    		System.out.printf("MEDIA = %.5f\n", mediaNota);
+    		
+    		entrada.close();
+ 
+    }
+}
