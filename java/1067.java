109f056f6709e131ebf351dacbc121cd59ba8d76
Gisele Bezerra de Almeida <110948857+GiseleBezerra@users.noreply.github.com>
2024-08-21 15:15:52 -0300

    1067.java

---
+++ java/1067.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    
+      Scanner entrada = new Scanner(System.in);
+      
+      int valor = entrada.nextInt();
+      
+      for (int i = 0; i <= valor; i++) {	
+        if (i % 2 == 1) {
+          System.out.println(i);
+      }
+    }
+    entrada.close();
+  }
+}
